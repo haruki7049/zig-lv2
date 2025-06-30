a6c3fc024ca518a493195c4b616d924b8a65d408
haruki7049 <tontonkirikiri@gmail.com>
2025-07-01 00:42:18 +0900

    Update build.zig syntaxes for Zig-0.14.0

--- build.zig
+++ build.zig
@@ -1,21 +1,46 @@
 const std = @import("std");
-const Builder = @import("std").build.Builder;
 
 const examples = &[_][]const u8{"amp", "fifths", "params"};
 
-pub fn build(b: *Builder) !void {
-    const mode = b.standardReleaseOptions();
-    inline for (examples) |example, i| {
-        const lib = b.addSharedLibrary(example, "examples/" ++ example ++ "/" ++ example ++ ".zig", .{ .unversioned = {} });
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-        lib.addPackagePath("lv2", "src/lv2.zig");
-        lib.setBuildMode(mode);
-        lib.setOutputDir("zig-out/" ++ example ++ ".lv2");
-        lib.linkLibC();
-        lib.addIncludeDir("lv2");
-        
-        var step = b.step(example, "Build example \"" ++ example ++ "\"");
-        step.dependOn(&b.addInstallFileWithDir("examples/" ++ example ++ "/" ++ example ++ ".ttl", .Prefix, example ++ ".lv2/manifest.ttl").step);
-        step.dependOn(&lib.step);
+    // Library module
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/lv2.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Library
+    const lib = b.addLibrary(.{
+        .linkage = .static,
+        .name = "lv2",
+        .root_module = lib_mod,
+    });
+
+    // Link lv2 library
+    lib.linkLibC();
+    lib.addIncludePath(b.path("lv2"));
+
+    // Unit tests for library
+    const lib_unit_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+
+    // Test step
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_lib_unit_tests.step);
+
+    // Example generate step
+    const examples_step = b.step("examples", "Build examples");
+    inline for (examples) |example| {
+        const source = b.path("examples/" ++ example ++ "/" ++ example ++ ".ttl");
+        const dest = example ++ ".lv2/mainfest.ttl";
+        examples_step.dependOn(&b.addInstallFileWithDir(source, .prefix, dest).step);
     }
 }
